fcf3130e0a06cc2bc7305a68401233e0490b139d
Spencer A. Gray <grayspa17@gmail.com>
2023-03-17 16:34:17 -0400

    init commit

---
+++ backend/email_scraper.py
@@ -0,0 +1,44 @@
+
+from redbox import EmailBox
+from redbox.query import UNSEEN, SUBJECT
+from redbox import gmail
+from bs4 import BeautifulSoup
+from pprint import pprint
+import os
+
+gmail.username = os.environ["GMAIL_EMAIL"]
+gmail.password = os.environ["GMAIL_APP_CODE"]
+search_subject = "Climb Youth Devotional Response"
+
+inbox = gmail['INBOX']
+
+tags = ['First Name', 'Last Name', 'Day', 'Reflection']
+
+for msg in inbox.search(SUBJECT(search_subject)):
+
+    print('START\n\n')
+
+    html = msg.html_body
+    parsed_html = BeautifulSoup(html, features="html.parser")
+    #print(parsed_html.prettify())
+    #print(parsed_html.find_all('br'))
+
+    export_info = {}
+
+    take_next = False
+    take_tag = ''
+    for string in parsed_html.strings:
+        
+        string = string.replace('=', '').strip()
+
+        if string == '':
+            continue
+
+        if take_next:
+            export_info[take_tag] = string
+            take_next = False
+        elif string in tags:
+            take_next = True
+            take_tag = string
+
+    pprint(export_info)
